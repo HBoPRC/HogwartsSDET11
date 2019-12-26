7f8b956ab184808c6cd38b1d268eaa4439fa471b
啵板 <37980474+HBoPRC@users.noreply.github.com>
2019-12-26 19:59:31 +0800

    add comment

--- unit/test_pytest_param.py
+++ unit/test_pytest_param.py
@@ -1,10 +1,13 @@
+import allure
 import pytest
 
-
+# 测试对象
 def div(a, b):
     return a / b
 
 
+# 参数化，这里验证正整数
+@allure.suite('正整数验证')
 @pytest.mark.parametrize('num1,num2,result', [
     (10, 2, 5),
     (6, 3, 2),
@@ -14,6 +17,8 @@ def test_div_int(num1, num2, result):
     assert div(num1, num2) == result
 
 
+# 这里验证负整数
+@allure.suite('负整数验证')
 @pytest.mark.parametrize('num1,num2,result', [
     (-10, 2, -5),
     (6, -3, -2),
@@ -23,6 +28,8 @@ def test_div_fuint(num1, num2, result):
     assert div(num1, num2) == result
 
 
+# 这里验证浮点数
+@allure.suite('浮点数验证')
 @pytest.mark.parametrize('num1,num2,result', [
     (1.1, 2, 0.55),
     (4, 0.5, 8),
@@ -32,6 +39,8 @@ def test_div_float(num1, num2, result):
     assert div(num1, num2) == result
 
 
+# 这里验证字符串
+@allure.suite('字符串验证')
 @pytest.mark.exception
 @pytest.mark.parametrize('num1,num2,result', [
     ('a', 2, ''),
@@ -39,13 +48,26 @@ def test_div_float(num1, num2, result):
     ('a', 'b', ''),
 ])
 def test_div_string(num1, num2, result):
-    assert div(num1, num2) == result
+    with pytest.raises(TypeError):
+        div(num1, num2)
 
 
+# 被除数为0
+@allure.suite('被除数0值验证')
 @pytest.mark.exception
 @pytest.mark.parametrize('num1,num2,result', [
     (0, 2, 0),
+])
+def test_div_string(num1, num2, result):
+    assert div(num1, num2) == result
+
+
+# 除数为0
+@allure.suite('除数0值验证')
+@pytest.mark.exception
+@pytest.mark.parametrize('num1,num2,result', [
     (4, 0, 0),
 ])
 def test_div_zero(num1, num2, result):
-    assert div(num1, num2) == result
+    with pytest.raises(ZeroDivisionError):
+        div(num1, num2)
